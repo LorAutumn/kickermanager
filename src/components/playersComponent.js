cff3ebac1a7bb24ca2e3bd6d8cfd9a9c6912f542
61251850 <LorAutumn@users.noreply.github.com>
2021-04-29 10:27:14 +0200

    added first players component with add function

--- src/components/playersComponent.js
+++ src/components/playersComponent.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function PlayersComponent() {
+    const [showNewPlayer, setShowNewPlayer] = useState(true)
+    const [newPlayerName, setNewPlayerName] = useState('')
+    const [newPlayerLocation, setNewPlayerLocation] = useState('')
+
+    console.log('name', newPlayerName, 'location', newPlayerLocation)
+
     return (
         <div className='players-wrapper'>
-            <div>Players</div>
+            <div>
+                <input
+                    type='text'
+                    placeholder='Player Name'
+                    onChange={e => setNewPlayerName(e.target.value)}
+                />
+                <input
+                    type='text'
+                    placeholder='Player Location'
+                    onChange={e => setNewPlayerLocation(e.target.value)}
+                />
+                <button onClick={() => setShowNewPlayer(!showNewPlayer)}>
+                    add new Player
+                </button>
+            </div>
         </div>
     )
 }
